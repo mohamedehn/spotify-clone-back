7d3034fc6e6ea4a2ec701a1422191dbe84773c2d
ehn <mohamed.ebarhmatin@yahoo.fr>
2024-05-08 15:24:29 +0200

    fix authresource

--- src/main/java/fr/ehn/spotifycloneback/usercontext/presentation/AuthResource.java
+++ src/main/java/fr/ehn/spotifycloneback/usercontext/presentation/AuthResource.java
@@ -11,6 +11,7 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,11 +44,12 @@ public class AuthResource {
     }
 
     //methode pour deconnecter un user
-    public ResponseEntity<?> logout(HttpServletRequest request){
-        String issueUri = registration.getProviderDetails().getIssuerUri();
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request) {
+        String issuerUri = registration.getProviderDetails().getIssuerUri();
         String originUrl = request.getHeader(HttpHeaders.ORIGIN);
-        Object[] params = {issueUri, registration.getClientId(), originUrl};
-        String logoutUrl = MessageFormat.format("{0}v2/logout?clientId={1}&returnTo={2}", params);
+        Object[] params = {issuerUri, registration.getClientId(), originUrl};
+        String logoutUrl = MessageFormat.format("{0}v2/logout?client_id={1}&returnTo={2}", params);
         request.getSession().invalidate();
         return ResponseEntity.ok().body(Map.of("logoutUrl", logoutUrl));
     }
